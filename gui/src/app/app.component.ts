b9b35a10b8f99bacbd8988aef92fe5a3372f41c5
ayman-ms <ayma513@gmail.com>
2025-07-13 18:21:22 +0200

    started with home page and navbar componentes

--- gui/src/app/app.component.ts
+++ gui/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./features/auth/login/login.component";
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
-
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, NavbarComponent],
+  imports: [RouterOutlet, LoginComponent, NavbarComponent, ButtonModule,CardModule,InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
