a223192f43e2f1d105222c236626d3f61b802c63
ayman-ms <ayma513@gmail.com>
2025-08-03 18:06:02 +0200

    ended add form

--- gui/src/app/features/properties/add/add.component.ts
+++ gui/src/app/features/properties/add/add.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add.component.html',
   styleUrl: './add.component.scss'
 })
 export class AddComponent {
+  zipCode: string = '';
+  country: string = 'de'; // 'us' or 'de'
+  state: string = '';
+  city: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  lookupZip() {
+    if (!this.zipCode || this.zipCode.length < 4) return;
+
+    const url = `https://api.zippopotam.us/${this.country}/${this.zipCode}`;
+    this.http.get<any>(url).subscribe({
+      next: (res) => {
+        const place = res.places[0];
+        this.city = place['place name'];
+        this.state = place['state'];
+      },
+      error: () => {
+        this.city = '';
+        this.state = '';
+        alert('ZIP code not found for ' + this.country.toUpperCase());
+      }
+    });
+  }
+
+  onSubmit(form: any) {
+  if (form.valid) {
+    console.log("Form Submitted:", form.value);
+    // هنا أضف ما تريده مثلاً: إرسال البيانات إلى backend
+  } else {
+    console.warn("Form is invalid");
+  }
+}
 }
